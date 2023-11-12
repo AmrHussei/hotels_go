7cd2a513223455a278549b19261af763b1386e44
Amr Hussein <94804979+AmrHussei@users.noreply.github.com>
2023-11-13 01:24:32 +0200

    refactor clip path

--- lib/widgets/body_widgets/center_content_widget.dart
+++ lib/widgets/body_widgets/center_content_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hotels_go/core/utils/app_colors.dart';
+import 'package:hotels_go/widgets/body_widgets/clip_path_widget.dart';
 import 'package:hotels_go/widgets/body_widgets/date_range_widget.dart';
 import 'package:hotels_go/widgets/body_widgets/dropdown_button_form_field_widget.dart';
 import 'package:hotels_go/widgets/body_widgets/find_hotels_button_widget.dart';
 
 import '../../core/utils/constant.dart';
 import '../../core/widgets/text_form_field_widget.dart';
-import '../../screens/hotels_search.dart';
 
 class CenterContentWidget extends StatefulWidget {
   const CenterContentWidget({super.key});
@@ -24,62 +24,64 @@ class _CenterContentWidgetState extends State<CenterContentWidget> {
     return Center(
       child: Padding(
         padding: EdgeInsets.all(16.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const ClipPathWidget(),
-            5.verticalSpace,
-            Container(
-              height: 300.h,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(18.sp)),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [AppColors.primaryShade, AppColors.primary],
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ClipPathWidget(),
+              5.verticalSpace,
+              Container(
+                height: 300.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(18.sp)),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [AppColors.primaryShade, AppColors.primary],
+                          begin: Alignment.centerRight,
+                          end: Alignment.centerLeft,
+                        ),
+                        borderRadius: BorderRadius.circular(18.sp),
                       ),
-                      borderRadius: BorderRadius.circular(18.sp),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 12.w),
-                      child: Column(
-                        children: [
-                          14.verticalSpace,
-                          TextFormFieldWidget(
-                            controller: selectCityController,
-                            fillColor: Colors.white,
-                            filled: true,
-                            hint: 'Select City',
-                          ),
-                          10.verticalSpace,
-                          const DateRangeWidget(),
-                          10.verticalSpace,
-                          DropdownButtonFormFieldWidget(
-                            itemList: nationalities,
-                            hintText: 'Select Nationality',
-                          ),
-                          10.verticalSpace,
-                          DropdownButtonFormFieldWidget(
-                            itemList: roomList,
-                            hintText: '1 Room, 1 Adult, 0 Children',
-                          ),
-                          14.verticalSpace
-                        ],
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.w),
+                        child: Column(
+                          children: [
+                            14.verticalSpace,
+                            TextFormFieldWidget(
+                              controller: selectCityController,
+                              fillColor: Colors.white,
+                              filled: true,
+                              hint: 'Select City',
+                            ),
+                            10.verticalSpace,
+                            const DateRangeWidget(),
+                            10.verticalSpace,
+                            DropdownButtonFormFieldWidget(
+                              itemList: nationalities,
+                              hintText: 'Select Nationality',
+                            ),
+                            10.verticalSpace,
+                            DropdownButtonFormFieldWidget(
+                              itemList: roomList,
+                              hintText: '1 Room, 1 Adult, 0 Children',
+                            ),
+                            14.verticalSpace
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  const FindHotelsButtonWidget()
-                ],
+                    const FindHotelsButtonWidget()
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/body_widgets/clip_path_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hotels_go/core/utils/app_colors.dart';
+import 'package:hotels_go/core/widgets/text_utils.dart';
+
+class ClipPathWidget extends StatelessWidget {
+  const ClipPathWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: ClipperWidget(),
+      child: Container(
+        padding: EdgeInsets.all(8.w),
+        width: 0.5.sw,
+        height: 50.h,
+        color: AppColors.primary,
+        alignment: AlignmentDirectional.centerStart,
+        child: TextUtils(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w600,
+          text: 'Hotels Search',
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class ClipperWidget extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(size.width - 60, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    path.lineTo(0, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
